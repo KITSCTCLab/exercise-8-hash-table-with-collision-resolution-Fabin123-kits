8e4c1c44d6839ccbe753c0435fe6407f65996bbf
Fabin123-kits <110010882+Fabin123-kits@users.noreply.github.com>
2022-09-20 11:56:20 +0530

    fab8

--- Main.py
+++ Main.py
@@ -2,13 +2,19 @@ import re
 
 def display_hash(hashtable) -> None:
 	# Write your code here
-
+	for i in range(len(Hashtable)):
+		print(i,end=" ")
+		for j in (Hashtable[i]):
+			print("-->",end=" ")
+			print(j,end=" ")
+		print()
 def Hashing(keyvalue) -> int:
 	return keyvalue % len(HashTable)
 
 def insert(Hashtable, keyvalue, value) -> None:
 	# Write your code here
-
+	hk = Hashing(keyvalue)
+	Hashtable[hk].append(value)
 
 # Do not edit the following code
 hash_table_size = int(input())
